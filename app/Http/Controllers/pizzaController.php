25275aa7305da94156451049e6eb5051dfbb0e47
I-Husillos <ivanhuslabra@gmail.com>
2026-01-14 12:20:53 +0100

    metodos crear almacenar y el formulario para crear la pizza y rutas

--- app/Http/Controllers/pizzaController.php
+++ app/Http/Controllers/pizzaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Pizza;
+use App\Models\Ingrediente;
 
 class pizzaController extends Controller
 {
@@ -20,4 +21,37 @@ class pizzaController extends Controller
 
         return view('pizzas.showOnePizza', compact('pizza'));
     }
+
+    public function create()
+    {
+        $ingredientes = Ingrediente::all();
+
+        return view('pizzas.create', compact('ingredientes'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'descripcion' => 'required',
+            'precio' => 'required|numeric',
+            'ingredientes' => 'array'
+        ],
+        [
+            'nombre.required' => 'El nombre es requerido',
+            'descripcion.required' => 'La descripcion es requerida',
+            'precio.required' => 'El precio es requerido',
+            'precio.numeric' => 'El precio debe ser numerico',
+            'ingredientes.array' => 'Los ingredientes son requeridos'
+        ]);
+
+        $pizza = Pizza::create($request->only(['nombre', 'descripcion', 'precio']));
+
+        if($request->has('ingredientes')){
+            $pizza->ingredientes()->attach($request->all('ingredientes'));
+        }
+
+        return redirect()->route('pizzas.showAllPizzas');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use App\Http\Controllers\pizzaController;
 
 Route::get('/', [pizzaController::class, 'showAllPizzas'])->name('pizzas.showAllPizzas');
 Route::get('/pizza/{id}', [pizzaController::class, 'showOnePizzas'])->name('pizzas.showOnePizza');
+
+
+Route::post('/pizzas', [pizzaController::class, 'store'])->name('pizzas.store');
+Route::get('/pizzas/create', [pizzaController::class, 'create'])->name('pizzas.create');
+
+Route::get('/pizzas/{id}/edit', [pizzaController::class, 'edit'])->name('pizzas.edit');
+Route::put('/pizzas/{id}', [pizzaController::class, 'update'])->name('pizzas.update');
+
